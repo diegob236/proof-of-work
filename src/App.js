e021f8ad5a7df9d754b5acdd9d8f77683acc67ef
diegob <diegob236@gmail.com>
2019-05-04 15:11:28 -0600

    Added dashboard route only accessible to logged in users

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { Component } from 'react';
+import { Route, Redirect } from "react-router-dom";
 
 import Welcome from './Welcome';
 import Scanner from './Scanner';
 import Login from './Login';
 import Signup from './SignUp';
+import Dashboard from './Dashboard';
 
 import './App.css';
 
 const uuidv3 = require('uuid/v3');
 
-function App() {
+function PrivateRoute ({component: Component, authed, ...rest}) {
   return (
-    <div className="App">
-      <Route path="/" exact component={Welcome} />
-      <Route path="/scan" component={Scanner} />
-      <Route path="/login" component={Login} />
-      <Route path="/signup" component={Signup} />
-    </div>
-  );
+    <Route
+      {...rest}
+      render={(props) => authed === true
+        ? <Component {...props} />
+        : <Redirect to={{pathname: '/login', state: {from: props.location}}} />}
+    />
+  )
+}
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      loggedIn: false
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Route path="/" exact component={Welcome} />
+        <Route path="/scan" component={Scanner} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <PrivateRoute authed={this.state.loggedIn} path='/dashboard' component={Dashboard} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Dashboard.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { Button, ButtonGroup } from "react-bootstrap";
+import './Dashboard.css';
+
+const uuidv3 = require('uuid/v3');
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div className="Dashboard">
+      
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
