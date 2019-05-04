1260b2fae8f774efc35a115ee9764b9c4c716737
Nic Deckhut <nicno90@hotmail.com>
2019-05-03 19:55:17 -0600

    Added more fields to Sign Up form

--- src/SignUp.js
+++ src/SignUp.js
@@ -15,7 +15,7 @@ export default class Signup extends Component {
     this.state = {
       $class: "org.pow.app.User",
       uID: "",
-      type: "",
+      type: "UNEMPLOYED",
       name: "",
       email: "",
       DOB: "",
@@ -23,13 +23,75 @@ export default class Signup extends Component {
     };
   }
 
+validateForm() {
+  return this.state.email.length > 0 &&
+    this.state.name.length > 0 &&
+    this.state.DOB > 0 &&
+    this.state.phone > 9;
+}
+
+handleChange = event => {
+  this.setState({
+    [event.target.id]: EventTarget.value
+  });
+}
+
+handleSubmit = event => {
+  
+}
+
   renderForm(){
     return(
       <form onSubmit={this.handleSubmit()}>
-        <FormGroup controlId='email'>
-          <FormLabel>Email Adress</FormLabel>
-          <ControlLabel type="email" placeholder="Enter email" />
+        <FormGroup controlId='name'>
+          <Form.Label>Name</Form.Label>
+          <ControlLabel type="name" placeholder="Enter your first and last name" />
+          <Form.Control
+              autoFocus
+              type="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
         </FormGroup>
+        <FormGroup controlId='email'>
+          <Form.Label>Email Adress</Form.Label>
+          <ControlLabel type="email" placeholder="Enter email" />
+          <Form.Control
+              autoFocus
+              type="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+        </FormGroup>
+        
+        <FormGroup controlId='phone'>
+          <Form.Label>Phone Number</Form.Label>
+          <ControlLabel type="phone" placeholder="" />
+          <Form.Control
+              autoFocus
+              type="phone"
+              value={this.state.phone}
+              onChange={this.handleChange}
+            />
+        </FormGroup>
+        <FormGroup controlId='DOB'>
+          <Form.Label>Date of Birth</Form.Label>
+          <ControlLabel type="DOB" placeholder="" />
+          <Form.Control
+              autoFocus
+              type="phone"
+              value={this.state.phone}
+              onChange={this.handleChange}
+            />
+        </FormGroup>
+        <Button
+            block
+            bsSize="large"
+            disabled={!this.validateForm()}
+            type="submit"
+          >
+            Sign Up
+          </Button>
       </form>
     );
   }
