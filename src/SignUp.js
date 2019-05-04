f43ac3b2e348714278daab9f9427614afb4e49a7
Nic Deckhut <nicno90@hotmail.com>
2019-05-03 21:21:08 -0600

    Fixed SignUp

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,14 +1,12 @@
 import React, { Component } from "react";
-import {
-  Button,
-  FormGroup,
-  FormControl,
-  ControlLabel
-} from "react-bootstrap";
+import { Button, Panel, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 
 import "./SignUp.css";
 
-export default class Signup extends Component {
+const axios = require('axios')
+const uuidv3 = require('uuid/v3');
+
+class Signup extends Component {
   constructor(props) {
     super(props);
 
@@ -20,79 +18,104 @@ export default class Signup extends Component {
       email: "",
       DOB: "",
       phone: "",
+      company: {}
     };
   }
 
-validateForm() {
-  return this.state.email.length > 0 &&
-    this.state.name.length > 0 &&
-    this.state.DOB > 0 &&
-    this.state.phone > 9;
-}
+  validateForm() {
+    return this.state.email.length > 0 &&
+      this.state.name.length > 0 &&
+      this.state.DOB.length == 10 &&
+      this.state.phone.length > 9;
+  }
 
-handleChange = event => {
-  this.setState({
-    [event.target.id]: EventTarget.value
-  });
-}
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
 
-handleSubmit = event => {
-  
-}
+  handleDate = event => {
+    this.setState({
+      DOB: event.target.value
+    });
+  }
 
-  renderForm(){
+  handleSubmit = event => {
+    var hash = uuidv3(this.state.email, uuidv3.URL)
+    var date = this.state.DOB +'T00:00:00.000Z'
+    this.setState({
+      uID: hash,
+      DOB: date
+    });
+
+    axios({
+      method: 'post',
+      url: 'http://157.230.172.148:3000/',
+      data: this.state
+    })
+      .then(function (response){
+        console.log(response);
+      })
+      .catch(function (error){
+        console.log(error);
+      })
+    }
+
+  render() {
     return(
-      <form onSubmit={this.handleSubmit()}>
-        <FormGroup controlId='name'>
-          <Form.Label>Name</Form.Label>
-          <ControlLabel type="name" placeholder="Enter your first and last name" />
-          <Form.Control
-              autoFocus
-              type="name"
-              value={this.state.name}
-              onChange={this.handleChange}
-            />
-        </FormGroup>
-        <FormGroup controlId='email'>
-          <Form.Label>Email Adress</Form.Label>
-          <ControlLabel type="email" placeholder="Enter email" />
-          <Form.Control
-              autoFocus
-              type="email"
-              value={this.state.email}
-              onChange={this.handleChange}
-            />
-        </FormGroup>
-        
-        <FormGroup controlId='phone'>
-          <Form.Label>Phone Number</Form.Label>
-          <ControlLabel type="phone" placeholder="" />
-          <Form.Control
-              autoFocus
-              type="phone"
-              value={this.state.phone}
-              onChange={this.handleChange}
-            />
-        </FormGroup>
-        <FormGroup controlId='DOB'>
-          <Form.Label>Date of Birth</Form.Label>
-          <ControlLabel type="DOB" placeholder="" />
-          <Form.Control
-              autoFocus
-              type="phone"
-              value={this.state.phone}
-              onChange={this.handleChange}
-            />
-        </FormGroup>
-        <Button
-            block
-            bsSize="large"
-            disabled={!this.validateForm()}
-            type="submit"
-          >
-            Sign Up
+      <div className='SignUp'>
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup controlId='name'>
+            <ControlLabel>Name</ControlLabel>
+            <FormControl
+                autoFocus
+                type="name"
+                value={this.state.name}
+                onChange={this.handleChange}
+              />
+          </FormGroup>
+          <FormGroup controlId='email'>
+            <ControlLabel>Email Adress</ControlLabel>
+            <FormControl
+                autoFocus
+                type="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+          </FormGroup>
+          
+          <FormGroup controlId='phone'>
+            <ControlLabel>Phone Number</ControlLabel>
+            <FormControl
+                autoFocus
+                type="phone"
+                value={this.state.phone}
+                onChange={this.handleChange}
+              />
+          </FormGroup>
+          <FormGroup controlId='DOB'>
+            <ControlLabel>Date of Birth</ControlLabel>
+            <FormControl
+                autoFocus
+                type="DOB"
+                value={this.state.DOB}
+                placeholder='YYYY-MM-DD'
+                onChange={this.handleDate}
+              />
+          </FormGroup>
+          <Button
+              block
+              bsSize="large"
+              disabled={!this.validateForm()}
+              type="submit"
+            >
+              Sign Up
           </Button>
-      </form>
+        </form>
+      </div>
     );
   }
 }
+
+export default Signup;
