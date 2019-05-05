ee5560fb3fbb9423aed570f3b43399ce6248a455
Nic Deckhut <nicno90@hotmail.com>
2019-05-04 21:28:43 -0600

    Imported Resume.js in Dashboard.js

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Button, ButtonGroup } from "react-bootstrap";
 import './Dashboard.css';
+import Resume from './Resume.js'
 
 const uuidv3 = require('uuid/v3');
 
