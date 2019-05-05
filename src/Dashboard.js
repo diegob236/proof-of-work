01b2e2a167b7795b346a8e215472c132e2cb7ac0
diegob <diegob236@gmail.com>
2019-05-04 20:08:08 -0600

    Added logout and dashboard buttons (navbar needs fixing)

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,14 +1,27 @@
 import React, { Component } from "react";
 import { Button, ButtonGroup } from "react-bootstrap";
 import './Dashboard.css';
+import DashboardNavbar from './DashboardNavbar';
 
 const uuidv3 = require('uuid/v3');
 
 class Dashboard extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
-      <div className="Dashboard">
-      
+      <div>
+        <DashboardNavbar logOut={this.props.logOut}></DashboardNavbar>
+        <div className="Dashboard">
+          <h1>Welcome back, {this.props.email}!</h1>
+          <Button className="menubutton" variant="dark">My Resume</Button>
+          <Button className="menubutton" variant="dark">Scan Resume</Button>
+          <Button className="menubutton" variant="dark">Hire Employee</Button>
+          <Button className="menubutton" variant="dark">Terminate Employee</Button>
+        </div>
       </div>
     );
   }
